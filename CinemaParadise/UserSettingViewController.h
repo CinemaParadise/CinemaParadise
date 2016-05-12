79c17e7da3015e778a65e1dc48e4b1f2b7f97b08
GraceLilac <lovelove8926@naver.com>
2016-05-13 00:28:23 +0900

    [CinemaParadise-2] 프로토타입

---
+++ CinemaParadise/UserSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserSettingViewController.h
+//  CinemaParadise
+//
+//  Created by GraceLilac on 2016. 5. 13..
+//  Copyright © 2016년 GraceLilac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserSettingViewController : UIViewController
+
+@end
