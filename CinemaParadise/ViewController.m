de96d9b0e61fb59424a1732d77cc6bed1747f59a
GraceLilac <lovelove8926@naver.com>
2016-05-15 03:09:24 +0900

    login api 적용

--- CinemaParadise/ViewController.m
+++ CinemaParadise/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 #import <KakaoOpenSDK/KakaoOpenSDK.h>
+#import "UIButton+Addition.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *loginButton;
 
 @end
 
@@ -17,9 +20,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self.loginButton setBackgroundColor:[UIColor colorWithRed:0xff / 255.0 green:0xcc / 255.0 blue:0x00 / 255.0 alpha:1.0] forState:UIControlStateNormal];
+    [self.loginButton setBackgroundColor:[UIColor colorWithRed:0xff / 255.0 green:0xb4 / 255.0 blue:0x00 / 255.0 alpha:1.0] forState:UIControlStateHighlighted];
+    self.loginButton.clipsToBounds = YES;
+    self.loginButton.layer.cornerRadius = 4;
+
+}
+- (IBAction)login:(id)sender {
+    KOSession *session = [KOSession sharedSession];
     
-//    KOLoginButton *loginButton = [[KOLoginButton alloc] initWithFrame:CGRectMake(0, 0, 300, 100)];
-//    [self.view addSubview:loginButton];
+    if (session.isOpen) {
+        [session close];
+    }
+    
+    session.presentingViewController = self.navigationController;
+    [session openWithCompletionHandler:^(NSError *error) {
+        session.presentingViewController = nil;
+        
+        if (!session.isOpen) {
+            [[[UIAlertView alloc] initWithTitle:@"에러" message:error.description delegate:nil cancelButtonTitle:@"확인" otherButtonTitles:nil, nil] show];
+        }
+    }];
+
 }
 
 @end
