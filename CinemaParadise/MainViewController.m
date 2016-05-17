7fe927125627ce55f28f3f5c0276ca5695785c70
GraceLilac <lovelove8926@naver.com>
2016-05-18 04:36:32 +0900

    main view 디자인

--- CinemaParadise/MainViewController.m
+++ CinemaParadise/MainViewController.m
@@ -12,7 +12,8 @@
 @interface MainViewController ()
 
 @property (nonatomic, strong) UIImageView *posterImageView;
-@property (nonatomic, strong) UILabel *presentingMessage;
+@property (nonatomic, strong) UILabel *presentingMessage1;
+@property (nonatomic, strong) UILabel *presentingMessage2;
 @property (strong, nonatomic) UIButton *searchingButton;
 
 @end
@@ -33,17 +34,24 @@
     self.posterImageView.translatesAutoresizingMaskIntoConstraints = NO;
     [self.view addSubview:self.posterImageView];
     
-    self.presentingMessage = [[UILabel alloc] init];
-    self.presentingMessage.text = @"최저가 영화 티켓 찾으러 가기!";
-    self.presentingMessage.font=[self.presentingMessage.font fontWithSize:25];
-    self.presentingMessage.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:self.presentingMessage];
+    
+    self.presentingMessage1 = [[UILabel alloc] init];
+    self.presentingMessage1.text = @"혜원님, 안녕하세요";
+    self.presentingMessage1.font=[self.presentingMessage1.font fontWithSize:27];
+    self.presentingMessage1.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.presentingMessage1];
+    
+    self.presentingMessage2 = [[UILabel alloc] init];
+    self.presentingMessage2.text = @"최저가 영화 티켓 찾으러 가기!";
+    self.presentingMessage2.font=[self.presentingMessage2.font fontWithSize:22];
+    self.presentingMessage2.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.presentingMessage2];
     
     self.searchingButton = [[UIButton alloc] init];
     [self.searchingButton setImage:[UIImage imageNamed:@"search_btn"] forState:UIControlStateNormal];
     self.searchingButton.translatesAutoresizingMaskIntoConstraints = NO;
     [self.searchingButton addTarget:self action:@selector(buttonClicked:) forControlEvents:UIControlEventTouchUpInside];
-    self.searchingButton.imageEdgeInsets = UIEdgeInsetsMake(110,110,110,110);
+    self.searchingButton.imageEdgeInsets = UIEdgeInsetsMake(114,114,114,114);
 
     [self.view addSubview:self.searchingButton];
     
@@ -54,7 +62,8 @@
 - (void)setupConstraints {
     NSDictionary *views = @{
                             @"imageView" : self.posterImageView,
-                            @"presentingMessage" : self.presentingMessage,
+                            @"presentingMessage1" : self.presentingMessage1,
+                            @"presentingMessage2" : self.presentingMessage2,
                             @"searchingButton" : self.searchingButton
                             };
     
@@ -67,19 +76,29 @@
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.posterImageView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1 constant:450]];
     
     
-    [self.view addConstraint:
-     [NSLayoutConstraint constraintWithItem:self.view attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.presentingMessage attribute:NSLayoutAttributeCenterX multiplier:1 constant:0]];
+    /* present message */
     
     [self.view addConstraints:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[imageView]-30-[presentingMessage(==35)]" options:0 metrics:nil views:views]];
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-35-[presentingMessage1]" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[imageView]-35-[presentingMessage1]" options:0 metrics:nil views:views]];
+    
+    
+    [self.view addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-35-[presentingMessage2]" options:0 metrics:nil views:views]];
+    
+    [self.view addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[presentingMessage1]-10-[presentingMessage2]" options:0 metrics:nil views:views]];
     
 
+    /* searching button */
     
     [self.view addConstraints:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[presentingMessage]-20-[searchingButton(==100)]" options:0 metrics:nil views:views]];
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[presentingMessage2][searchingButton(==100)]" options:0 metrics:nil views:views]];
     
     [self.view addConstraints:
-     [NSLayoutConstraint constraintsWithVisualFormat:@"H:[searchingButton(==100)]-25-|" options:0 metrics:nil views:views]];
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:[searchingButton(==100)]-30-|" options:0 metrics:nil views:views]];
     
 
     
